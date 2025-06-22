80e91a7c5fb0c90797390eaa49219f450a7c3cd1
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-21 21:28:40 -0600

    hello world for mapbox js to ts

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
 
 function Map() {
-  const mapRef = useRef();
-  const mapContainerRef = useRef();
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
 
   useEffect(() => {
+    if (map.current) return; // initialize map only once
+    
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw";
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
+    
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current!,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-74.5, 40],
+      zoom: 9,
     });
-
-    return () => {
-      mapRef.current.remove();
-    };
   }, []);
 
   return (
-    <>
-      <div id="map-container" ref={mapContainerRef} />
-    </>
+    <div ref={mapContainer} id="map-container" />
   );
 }
 
